fd3b3f33dfa2fc959aae0e5a4c1d7d271da680a8
Xemophon <maksimtrenev06@gmail.com>
2026-06-20 20:38:20 +0300

    Adjusting java env and making source files properly

---
+++ java exc/example.java
@@ -0,0 +1,16 @@
+public class example{
+    public static void main(String[] args) {
+        String[] best_cars = {"BMW", "Audi", "Mercedez"};
+        for(String car:best_cars){
+            System.out.println(car);
+        }
+        for(int i = 0; i<72; i+=5){
+            if(i==65){
+                i+=2;
+            }
+            System.out.println(i);
+            
+        }
+        //retarded a bit
+    }
+}
